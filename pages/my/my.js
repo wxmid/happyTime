42ba4f71b54fdd780fdeef155c219cdd18aa2505
stuart_1 <wuxmid@163.com>
2018-04-20 18:36:39 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,5 +1,47 @@
 // pages/my/my.js
+import * as echarts from '../../assets/ec-canvas/echarts';
 const app = getApp()
+
+function initChart(canvas, width, height) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    backgroundColor: "#ffffff",
+    color: ["#37A2DA", "#32C5E9", "#67E0E3"],
+    series: [{
+      name: '业务指标',
+      type: 'gauge',
+      detail: {
+        formatter: '{value}%'
+      },
+      axisLine: {
+        show: true,
+        lineStyle: {
+          width: 30,
+          shadowBlur: 0,
+          color: [
+            [0.3, '#67e0e3'],
+            [0.7, '#37a2da'],
+            [1, '#fd666d']
+          ]
+        }
+      },
+      data: [{
+        value: 60,
+        name: '当前飚速',
+      }]
+
+    }]
+  };
+
+  chart.setOption(option, true);
+
+  return chart;
+}
 
 Page({
 
@@ -7,7 +49,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo: {}
+    userInfo: {},
+    ec: {
+      onInit: initChart
+      }
     },
 
   /**
@@ -90,7 +135,12 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function (res) {
+    // return {
+    //   title: 'ECharts 可以在微信小程序中使用啦！',
+    //   path: '/pages/index/index',
+    //   success: function () { },
+    //   fail: function () { }
+    // }
   }
 })
