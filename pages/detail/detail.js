f5fb7418833856e5f66487d419ab8f89e74db772
wxmid <wuxmid@163.com>
2018-05-15 18:00:44 +0800

    pages/detail完善

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -8,6 +8,7 @@ Page({
   id:'1',
   x:360,
   y:336,
+  funny: {}
   },
 
   /**
@@ -15,11 +16,38 @@ Page({
    */
   onLoad: function (options) {
     let funnyArticleId = options.id ? options.id : '忍住别出声';
-    this.setData({
-      id: funnyArticleId
-    })
+    //根据id获取内容
+    let funnyStore = {
+      id: 'fn0001',
+      isOriginal: true,
+      headImg: '../../assets/img/head1.jpg',
+      nickName: 'GirlLog',
+      publishTime: '2018-04-19 10:52',
+      funnyStorylList: [
+        {
+          content: '老婆不喜欢家里的画眉鸟，所以平时鸟笼挂在阳台上，她不在家时我就拿进屋里逗一下。刚刚在家逗鸟，看到她下班回来了，我很自觉的把鸟笼拿去阳台，然后她说：“留意你很久了，每次我到家你就往阳台挂鸟笼，给谁发信号呢？！”我。。。',
+          image: '../../assets/img/gx1.jpg'
+        },
+        {
+          content: '电梯里，一女孩手里抱着东西，一屌丝男问：你几楼啊？女孩：9楼。屌丝男：你快按啊，一会儿过了！',
+          image: '../../assets/img/gx2.jpg'
+        },
+        {
+          content: '有次老公开好房间等我 ，我到了门外想逗逗他，就在门外小声的叫：306，你叫的小姐来了，开门！谁料隔壁的门打开了，一个男的出来对我说：等会儿完事了到我这边来一下啊！',
+          image: '../../assets/img/gx3.jpg'
+        }
+        ,
+        {
+          content: '婚礼，新郎：其实我在爱上你后，还爱过很多人。。。台下一片哗然，新娘也惊呆了。看到这情景，新郎满意的继续说：很多人，包括你的父母，你家人，你的朋友。。。台下掌声雷动。新娘顿了顿，问：也包括我的闺密吗？',
+          image: '../../assets/img/gx4.jpg'
+        }
+      ]
+    };
     wx.setNavigationBarTitle({
-      title: funnyArticleId
+      title: funnyStore.funnyStorylList[0].content
+    });
+    this.setData({
+      funny:funnyStore
     });
   },
 
@@ -99,4 +127,9 @@ Page({
       }
     }
   }, 
+  gotoIndex:function() {
+    wx.switchTab({
+      url: '../index/index',
+    })
+  }
 })
